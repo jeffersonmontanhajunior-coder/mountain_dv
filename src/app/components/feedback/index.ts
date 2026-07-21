36fcadea7796c7dcd1d527e54157562600a9f8f8
jeffersonmontanhajunior-coder <jeffersonmontanhajunior@gmail.com>
2026-07-21 15:10:07 -0300

    Delete src directory

--- src/app/components/feedback/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./EmptyState";
-export * from "./Skeleton";
-export * from "./Spinner";
-export * from "./Toast";
-export * from "./toast-store";
-export * from "./Tooltip";
--- src/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex min-h-[60vh] items-center justify-center">
-      <div className="h-10 w-10 animate-spin rounded-full border-4 border-border-default border-t-primary" />
-    </div>
-  );
-}
